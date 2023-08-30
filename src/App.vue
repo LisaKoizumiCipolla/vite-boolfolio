b43311aa0dad673f2b0330e0b065fbb9ac68b139
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-08-30 16:31:50 +0200

    Base created

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default{
+  name : 'App',
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
+  
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
